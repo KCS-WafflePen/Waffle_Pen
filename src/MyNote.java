0e5bba2f89b949d67ee33268bb5dd1166c26d424
iwantpark <phy9916@naver.com>
2024-01-04 16:51:07 +0900

    feat: 메인파일 수정

--- src/MyNote.java
+++ src/MyNote.java
@@ -3,20 +3,20 @@ import java.awt.event.*;
 import java.io.*;
 import java.awt.*;
 
-public class MyNote implements ActionListener{
+public class MyNote implements ActionListener {
     JFrame f;
     JMenuBar mb;
-    JMenu file,edit;
-    JMenuItem cut,copy,paste,selectAll;
+    JMenu file, edit;
+    JMenuItem cut, copy, paste, selectAll;
     JTextArea ta;
     JMenuItem i_save, i_load;
 
-    MyNote(){
-        f=new JFrame("자바 메모장");
-        cut=new JMenuItem("잘라내기");
-        copy=new JMenuItem("복사");
-        paste=new JMenuItem("붙이기");
-        selectAll=new JMenuItem("모두선택");
+    MyNote() {
+        f = new JFrame("자바 메모장");
+        cut = new JMenuItem("잘라내기");
+        copy = new JMenuItem("복사");
+        paste = new JMenuItem("붙이기");
+        selectAll = new JMenuItem("모두선택");
         i_save = new JMenuItem("저장");
         i_load = new JMenuItem("열기");
         cut.addActionListener(this);
@@ -27,38 +27,44 @@ public class MyNote implements ActionListener{
         i_load.addActionListener(this);
 
 
-        mb=new JMenuBar();
-        file=new JMenu("파일");
-        edit=new JMenu("편집");
+        mb = new JMenuBar();
+        file = new JMenu("파일");
+        edit = new JMenu("편집");
 
-        edit.add(cut);edit.add(copy);edit.add(paste);edit.add(selectAll);
-        file.add(i_save); file.add(i_load);
+        edit.add(cut);
+        edit.add(copy);
+        edit.add(paste);
+        edit.add(selectAll);
+        file.add(i_save);
+        file.add(i_load);
 
-        mb.add(file);mb.add(edit);
-        ta=new JTextArea();
-        ta.setBounds(5,5,360,320);
-        f.add(mb);f.add(ta);
+        mb.add(file);
+        mb.add(edit);
+        ta = new JTextArea();
+        ta.setBounds(5, 5, 360, 320);
+        f.add(mb);
+        f.add(ta);
         f.setJMenuBar(mb);
         f.setLayout(null);
-        f.setSize(400,400);
+        f.setSize(400, 400);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==cut)
+        if (e.getSource() == cut)
             ta.cut();
 
-        if(e.getSource()==paste)
+        if (e.getSource() == paste)
             ta.paste();
 
-        if(e.getSource()==copy)
+        if (e.getSource() == copy)
             ta.copy();
 
-        if(e.getSource()==selectAll)
+        if (e.getSource() == selectAll)
             ta.selectAll();
 
-        if(e.getSource()==i_save) {
+        if (e.getSource() == i_save) {
 
             FileDialog fd = new FileDialog(f, "저장", FileDialog.SAVE);
             fd.setVisible(true);
@@ -74,19 +80,18 @@ public class MyNote implements ActionListener{
                 fw.write(s);
                 fw.close();
             } catch (Exception e1) {
-                System.out.println("저장오류"+e1);
+                System.out.println("저장오류" + e1);
             }
         }
 
 
-
-        if(e.getSource()==i_load) {
+        if (e.getSource() == i_load) {
             FileDialog fd = new FileDialog(f, "열기", FileDialog.LOAD);
             fd.setVisible(true);
 
             String path = fd.getDirectory() + fd.getFile();
 
-            String s ="";
+            String s = "";
 
             if (fd.getFile() == null) return;
 
@@ -96,17 +101,17 @@ public class MyNote implements ActionListener{
 
                 int k;
 
-                while(true) {
+                while (true) {
                     k = fr.read();
-                    if(k == -1) break;
-                    s += (char)k;
+                    if (k == -1) break;
+                    s += (char) k;
                 }
 
                 fr.close();
 
             } catch (Exception e2) {
 
-                System.out.println("오류"+e2);
+                System.out.println("오류" + e2);
             }
 
             ta.setText(s);
@@ -115,7 +120,4 @@ public class MyNote implements ActionListener{
 
     }
 
-    public static void main(String[] args) {
-        new MyNote();
-    }
 }
