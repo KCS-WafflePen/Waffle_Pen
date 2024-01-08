0d0877daf914648dcee2504b262ec519c68ecce3
LeeJinyoung218 <amy021899@gmail.com>
2024-01-08 16:01:22 +0900

    fix: set Frame's minimum size and set MyBoard and Memo's size

--- src/board/MyNote.java
+++ src/board/MyNote.java
@@ -8,27 +8,18 @@ public class MyNote extends JPanel {
     Memo m = new Memo();
 
     public MyNote() {
-        this.setLayout(new BorderLayout());
+        mb.setPreferredSize(new Dimension(800, 600));
+        m.setPreferredSize(new Dimension(800, 150));
 
-        // MyBoard와 Memo를 수직으로 나누는 JSplitPane 생성
-        JSplitPane boardAndMemoSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, mb, m);
-        boardAndMemoSplitPane.setResizeWeight(0.5);
-
-        // 위에서 생성한 split pane과 Memo를 수평으로 나누는 JSplitPane 생성
-        JSplitPane mainSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, boardAndMemoSplitPane, m);
-        mainSplitPane.setResizeWeight(0.5);
-
-        // MyBoard와 위에서 생성한 split pane을 수직으로 나누는 JSplitPane 생성
-        JSplitPane finalSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, mb, mainSplitPane);
-        finalSplitPane.setResizeWeight(0.5);
-
-        // 최종적으로 구성된 split pane을 메인 패널에 추가
-        this.add(finalSplitPane, BorderLayout.CENTER);
+        this.setLayout(new FlowLayout());
+        this.add(mb);
+        this.add(m);
     }
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame();
-        jFrame.setSize(800, 600);
+        jFrame.setSize(800, 900);
+        jFrame.setMinimumSize(new Dimension(800, 750));
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.add(new MyNote());
         jFrame.setVisible(true);
