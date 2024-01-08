37a844d40121103d7d7b1c24676bf162023396ae
iwantpark <phy9916@naver.com>
2024-01-08 15:44:41 +0900

    feat: JSplitPane를 사용하여 메모장 하단배치

--- src/board/MyNote.java
+++ src/board/MyNote.java
@@ -10,14 +10,20 @@ public class MyNote extends JPanel {
     public MyNote() {
         this.setLayout(new BorderLayout());
 
-        // Create a JSplitPane with horizontal orientation
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, mb, m);
+        // MyBoard와 Memo를 수직으로 나누는 JSplitPane 생성
+        JSplitPane boardAndMemoSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, mb, m);
+        boardAndMemoSplitPane.setResizeWeight(0.5);
 
-        // Set the initial size distribution (50% each)
-        splitPane.setResizeWeight(0.5);
+        // 위에서 생성한 split pane과 Memo를 수평으로 나누는 JSplitPane 생성
+        JSplitPane mainSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, boardAndMemoSplitPane, m);
+        mainSplitPane.setResizeWeight(0.5);
 
-        // Add the split pane to the main panel
-        this.add(splitPane, BorderLayout.CENTER);
+        // MyBoard와 위에서 생성한 split pane을 수직으로 나누는 JSplitPane 생성
+        JSplitPane finalSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, mb, mainSplitPane);
+        finalSplitPane.setResizeWeight(0.5);
+
+        // 최종적으로 구성된 split pane을 메인 패널에 추가
+        this.add(finalSplitPane, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
